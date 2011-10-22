6f32ad459080ad17ff241140b13e8b4cd2ab5fb3
atinm <atinm.dev@gmail.com>
2011-10-22 14:12:13 -0400

    Added teamhacksung Makefile and build script

--- build.sh
+++ build.sh
@@ -1,45 +1,73 @@
-#!/bin/bash
+#!/bin/bash -x
 
-################################################################################
-#
-# Build Script
-#
-# Copyright Samsung Electronics(C), 2010
-#
-################################################################################
+setup ()
+{
+    if [ x = "x$ANDROID_BUILD_TOP" ] ; then
+        echo "Android build environment must be configured"
+        exit 1
+    fi
+    . "$ANDROID_BUILD_TOP"/build/envsetup.sh
 
+    KERNEL_DIR="$(dirname "$(readlink -f "$0")")"
+    BUILD_DIR="$KERNEL_DIR/build"
+    MODULES=("drivers/bluetooth/bthid/bthid.ko" "drivers/samsung/fm_si4709/Si4709_driver.ko" "drivers/samsung/j4fs/j4fs.ko" "drivers/media/video/gspca/gspca_main.ko" "drivers/scsi/scsi_wait_scan.ko" "drivers/net/wireless/bcm4330/dhd.ko" "drivers/samsung/vibetonz/vibrator.ko" "fs/cifs/cifs.ko")
 
-################################################################################
-# Useage
-#   : ./build.sh [mod]
-#
-# Example
-#   : ./build.sh eng
-################################################################################
-if	[ "" = "$1" ]
-then
-	echo --------------------------------------------------------------------------------
-	echo - Useage
-	echo -   : ./build.sh [mod]
-	echo -
-	echo - Example
-	echo -   : ./build.sh eng
-	echo --------------------------------------------------------------------------------
-	exit
+    if [ x = "x$NO_CCACHE" ] && ccache -V &>/dev/null ; then
+        CCACHE=ccache
+        CCACHE_BASEDIR="$KERNEL_DIR"
+        CCACHE_COMPRESS=1
+        CCACHE_DIR="$BUILD_DIR/.ccache"
+        export CCACHE_DIR CCACHE_COMPRESS CCACHE_BASEDIR
+    else
+        CCACHE=""
+    fi
+
+    CROSS_PREFIX="$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-"
+}
+
+build ()
+{
+    local target=$1
+    echo "Building for $target"
+    local target_dir="$BUILD_DIR/$target"
+    local module
+    [ x = "x$NO_RM" ] && rm -fr "$target_dir"
+    mkdir -p "$target_dir/usr"
+    cp "$KERNEL_DIR/usr/"*.list "$target_dir/usr"
+    sed "s|usr/|$KERNEL_DIR/usr/|g" -i "$target_dir/usr/"*.list
+    [ x = "x$NO_DEFCONFIG" ] && mka -C "$KERNEL_DIR" O="$target_dir" c1_${target}_defconfig HOSTCC="$CCACHE gcc"
+    if [ x = "x$NO_BUILD" ] ; then
+        mka -C "$KERNEL_DIR" O="$target_dir" HOSTCC="$CCACHE gcc" CROSS_COMPILE="$CCACHE $CROSS_PREFIX" modules
+        mka -C "$KERNEL_DIR" O="$target_dir" HOSTCC="$CCACHE gcc" CROSS_COMPILE="$CCACHE $CROSS_PREFIX" zImage
+        cp "$target_dir"/arch/arm/boot/zImage $ANDROID_BUILD_TOP/device/samsung/$target/kernel
+        for module in "${MODULES[@]}" ; do
+            cp "$target_dir/$module" $ANDROID_BUILD_TOP/device/samsung/$target
+        done
+    fi
+}
+    
+setup
+
+if [ "$1" = clean ] ; then
+    rm -fr "$BUILD_DIR"/*
+    exit 0
 fi
-export CPU_NUMBER=16
-export TARGET_BUILD_VARIANT=$1
 
+targets=("$@")
+if [ 0 = "${#targets[@]}" ] ; then
+    targets=(galaxys2)
+fi
 
+START=$(date +%s)
 
-################################################################################
-# Common Path
-################################################################################
-TOP_DIR=$PWD
+for target in "${targets[@]}" ; do 
+    build $target
+done
 
-# set root path
-export PLATFORMPATH="$TOP_DIR"
-
-cd $PLATFORMPATH 
-make update-api
-make -j$CPU_NUMBER PRODUCT-generic-$TARGET_BUILD_VARIANT
+END=$(date +%s)
+ELAPSED=$((END - START))
+E_MIN=$((ELAPSED / 60))
+E_SEC=$((ELAPSED - E_MIN * 60))
+printf "Elapsed: "
+[ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
+printf "%d sec(s)\n" $E_SEC
